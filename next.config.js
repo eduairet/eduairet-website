8ac487b3e15edb7cf187f3223ff3771fd6439b65
eduairet <hola@eduairet.com>
2024-02-08 22:53:52 -0600

    PWA support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+// eslint-disable-next-line unused-imports/no-unused-vars
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const path = require('node:path');
 
 /** @type {import('next').NextConfig} */
