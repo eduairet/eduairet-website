f9b2298b73f056eeb2cec05a2034556160400e17
eduairet <hola@eduairet.com>
2024-02-11 15:27:51 -0600

    default centered sketch

--- src/components/art/sketches/sketch02.ts
+++ src/components/art/sketches/sketch02.ts
@@ -6,28 +6,30 @@ import { FRAME_RATE, SKETCH_CONTAINER_ID } from '@/utils/client';
 import type P5 from 'p5';
 
 const sketch01 = (p: P5) => {
-  let theme: string;
+  let x: number, y: number;
+  const mainColor = (a: number, theme: string) =>
+    theme === 'light' ? `rgba(237,237,237,${a})` : `rgba(0,0,0,${a})`;
 
   p.setup = () => {
     const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
     canvas.parent(SKETCH_CONTAINER_ID);
     p.frameRate(FRAME_RATE);
+    x = p.width / 2;
+    y = p.height / 2;
   };
 
   p.draw = () => {
-    theme = localStorage.getItem('theme') || 'dark';
     p.clear();
     p.noFill();
-    p.fill(theme === 'dark' ? '#ededed' : '#000');
     p.noStroke();
-    for (let i = 0; i < 40; i++) {
+    // TODO handle mouse move
+    //x = p.mouseX;
+    //y = p.mouseY;
+    for (let i = 0; i < 200; i++) {
       const angle = p.frameCount / 9 + (i * p.PI) / 2;
-      p.ellipse(
-        p.mouseX + 5 * p.cos(angle),
-        p.mouseY + 5 * p.sin(angle),
-        100,
-        100
-      );
+      const o = p.lerp(0.00001, 0, i / 1);
+      p.fill(mainColor(o, localStorage.getItem('theme') || 'dark'));
+      p.ellipse(x + 5 * p.cos(angle), y + 5 * p.sin(angle), 5 * i, 5 * i);
     }
   };
 
