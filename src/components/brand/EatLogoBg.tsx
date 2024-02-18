bb097829f3bfc93615d6576280f44ba12c94d328
eduairet <hola@eduairet.com>
2024-02-17 20:27:07 -0600

    Hamburger Button Animation Added

--- src/components/brand/EatLogoBg.tsx
+++ src/components/brand/EatLogoBg.tsx
@@ -1,13 +1,10 @@
 import styles from './EatLogoBg.module.scss';
-import RotationTransition from '../transitions/RotationTransition';
 import EatLogo from '@/components/brand/EatLogo';
 
 export default function EatLogoBg() {
   return (
-    <RotationTransition>
-      <picture className={styles.picture}>
-        <EatLogo />
-      </picture>
-    </RotationTransition>
+    <picture className={styles.picture}>
+      <EatLogo />
+    </picture>
   );
 }
--- src/components/ui/HamburgerButton/HamburgerButton.tsx
+++ src/components/ui/HamburgerButton/HamburgerButton.tsx
@@ -1,9 +1,15 @@
 'use client';
 
 import { memo, useContext } from 'react';
+import { motion } from 'framer-motion';
 import styles from './HamburgerButton.module.scss';
 import IconButtonScreenTitle from '@/components/ui/IconButton/IconButtonScreenTitle';
 import { LanguageContext } from '@/store/LanguageProvider';
+
+const paths = {
+  top: { open: { d: 'M5 5L25 25' }, closed: { d: 'M5 8L25 8' } },
+  bottom: { open: { d: 'M5 25L25 5' }, closed: { d: 'M5 22L25 22' } },
+};
 
 interface IProps {
   isActive: boolean;
@@ -21,21 +27,38 @@ function HamburgerButton({ isActive, onClick }: IProps) {
       className={styles.hamburger}
       onClick={onClick}
     >
-      <span
-        className={[
-          styles['hamburger-lines'],
-          isActive ? styles.active : '',
-        ].join(' ')}
+      <svg
+        width='30'
+        height='30'
+        viewBox='0 0 30 30'
+        focusable={false}
+        xmlSpace='preserve'
+        aria-hidden
       >
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
+        <motion.path
+          d={isActive ? paths.top.open.d : paths.top.closed.d}
+          initial={false}
+          animate={isActive ? 'open' : 'closed'}
+          variants={paths.top}
+        />
+        <motion.path
+          d='M5 15L25 15'
+          animate={isActive ? 'open' : 'closed'}
+          variants={{
+            open: { opacity: 0 },
+            closed: { opacity: 1 },
+          }}
+        />
+        <motion.path
+          d={isActive ? paths.bottom.open.d : paths.bottom.closed.d}
+          initial={false}
+          animate={isActive ? 'open' : 'closed'}
+          variants={paths.bottom}
+        />
+      </svg>
       <IconButtonScreenTitle title={content.nav.menu} />
     </button>
   );
 }
-
-HamburgerButton.displayName = 'HamburgerButton';
 
 export default memo(HamburgerButton);
