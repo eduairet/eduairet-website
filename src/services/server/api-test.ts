295b5fee1a4484b8dc51a3c37dcfb466dade966b
eduairet <hola@eduairet.com>
2024-02-22 21:43:46 -0600

    API server test removed

--- src/services/server/api-test.ts
+++
@@ -1,7 +0,0 @@
-import { getHost, fetchData } from '@/utils/server';
-
-export const checkHealth = async () => {
-  const host = getHost();
-  const res = await fetchData<string>(`${host}/api/`);
-  return res.message;
-};
