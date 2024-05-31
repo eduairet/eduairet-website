56d88e40411ef4890859335ae1ec739716436dc2
eduairet <hola@eduairet.com>
2024-05-30 18:51:45 -0600

    Remove sketch due to performance issues on Safari

--- src/components/wrappers/BodyWrapper.tsx
+++ src/components/wrappers/BodyWrapper.tsx
@@ -1,16 +1,11 @@
 import { ReactNode } from 'react';
-import Art from '@/components/art/Art';
 
 interface IProps {
   children: ReactNode;
   hasSketch?: boolean;
 }
 
-export default function BodyWrapper({ children, hasSketch = true }: IProps) {
-  return (
-    <body>
-      {hasSketch && <Art />}
-      {children}
-    </body>
-  );
+export default function BodyWrapper({ children }: IProps) {
+  // TODO Add {hasSketch && <Art />} When performance issues are addressed
+  return <body>{children}</body>;
 }
