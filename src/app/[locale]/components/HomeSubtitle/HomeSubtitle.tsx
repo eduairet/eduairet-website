8192eb9c84bf0913d3cfbf143a8125adff57eb37
Eduardo Aire <hola@eduairet.com>
2025-09-17 01:13:26 -0600

    Update subtitle formatting and dictionary text
    Changed subtitle splitting from '/' to space in HomeSubtitle component and adjusted related SCSS styles for improved layout. Updated English and Spanish dictionary entries to use 'Front End Software Engineer' phrasing for consistency.

--- src/app/[locale]/components/HomeSubtitle/HomeSubtitle.tsx
+++ src/app/[locale]/components/HomeSubtitle/HomeSubtitle.tsx
@@ -7,7 +7,7 @@ interface IProps {
 }
 
 export default function HomeSubtitle({ subtitle }: IProps) {
-  const subtitleArray = subtitle.split('/');
+  const subtitleArray = subtitle.split(' ');
 
   return (
     <h2 className={styles.subtitle}>
